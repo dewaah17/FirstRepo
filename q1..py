47769e7721138c26743ed8a65ca643167c054d22
unknown <9923103117>
2025-08-06 10:51:04 +0530

    Initial commit

---
+++ q1..py
@@ -0,0 +1,6 @@
+friends = ["Aarav", "Bhavna", "Chirag", "Deepika", "Eshan", "Farhan", "Gauri", "Harsh", "Isha", "Jay"]
+print("First name:", friends[0])
+print("Last name:", friends[-1])
+print("Names from index 3 to 5:", friends[3:6])
+print("Names in reverse order:", friends[::-1])
+print("Names from index 8 to 3:", friends[8:2:-1])
